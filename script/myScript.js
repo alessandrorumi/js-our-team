0bf8600cbcb67e9d6c637bce838ef04a4e71a1ca
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-01 14:40:41 +0100

    Add: Creazione array

--- script/myScript.js
+++ script/myScript.js
@@ -1,3 +1,45 @@
 // Creare l’array di oggetti con le informazioni fornite.
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 // Stampare le stesse informazioni su DOM sotto forma di stringhe
+
+const team = [
+
+  {
+    'name': 'Wayne Barnett',
+    'role': 'Fouder & CEO',
+    'image': 'wayne-barnett-founder-ceo.jpg'
+  },
+
+  {
+    'name': 'Angela Caroll',
+    'role': 'Chief Editor',
+    'image': 'angela-caroll-chief-editor.jpg'
+  },
+
+  {
+    'name': 'Walter Gordon',
+    'role': 'Office Manager',
+    'image': 'walter-gordon-office-manager.jpg'
+  },
+
+  {
+    'name': 'Angela Lopez',
+    'role': 'Social Media Manager',
+    'image': 'angela-lopez-social-media-manager.jpg'
+  },
+
+  {
+    'name': 'Scott Estrada',
+    'role': 'Developer',
+    'image': 'scott-estrada-developer.jpg'
+  },
+
+  {
+    'name': 'Barbara Ramos',
+    'role': 'Graphic Designer',
+    'image': 'barbara-ramos-graphic-designer.jpg'
+  }
+  
+];
+
+console.log(team);
